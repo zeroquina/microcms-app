38e42061775b7738a54e1198948de95dd5a2b7fa
zeroquina <zeroquina@gmail.com>
2026-06-25 08:09:51 +0900

    feat: section 4-4-4まで完了

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.css";
+import Menu from "../Menu";
 
 export default function Header() {
   return (
@@ -15,19 +16,7 @@ export default function Header() {
           priority
         />
       </Link>
-      <nav className={styles.nav}>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/news">ニュース</Link>
-          </li>
-          <li>
-            <Link href="/members">メンバー</Link>
-          </li>
-          <li>
-            <Link href="/contact">お問い合わせ</Link>
-          </li>
-        </ul>
-      </nav>
+      <Menu />
     </header>
   );
 }
